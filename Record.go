262c2b66cbb8dae8c7ed4b5b06f0d06b6b7fa16f
Sinevia <info@sinevia.com>
2024-11-28 18:13:00 +0000

    upgraded carbon

--- Record.go
+++ Record.go
@@ -3,7 +3,7 @@ package sqlfilestore
 import (
 	"strings"
 
-	"github.com/golang-module/carbon/v2"
+	"github.com/dromara/carbon/v2"
 	"github.com/gouniverse/dataobject"
 	"github.com/gouniverse/sb"
 	"github.com/gouniverse/uid"
